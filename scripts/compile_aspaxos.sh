982825573c8f5d7906a4a3a426c9b2c77f8e941c
David Chu <davidchuyaya@gmail.com>
2020-12-01 18:53:33 -0800

    Old build file is unnecessary

--- scripts/compile_aspaxos.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install -y unzip git ca-certificates net-tools curl apt-transport-https libpq-dev libzmq3-dev curl libprotobuf-dev protobuf-compiler software-properties-common
-sudo snap install cmake --classic
-sudo wget https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-all-3.13.0.zip
-sudo unzip protobuf-all-3.13.0.zip -d /usr/local
-sudo cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++ . -B build
-sudo cmake --build build --target all -- -j 2
