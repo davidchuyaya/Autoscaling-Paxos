36793075f7889503b6ee2e3cb353b3962cea0971
David Chu <davidchuyaya@gmail.com>
2020-12-03 11:55:29 -0800

    Uncache AWS?

--- scripts/install_dependencies_for_building.sh
+++ scripts/install_dependencies_for_building.sh
@@ -2,4 +2,4 @@
 
 scripts/install_dependencies_for_running.sh
 sudo apt-get install -y build-essential
-sudo snap install cmake --channel=3.17/stable --classic
+sudo snap install cmake --channel=3.18/stable --classic
